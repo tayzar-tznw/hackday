b23f5ed11ed3e90dbfb0cb074f2e7351015c99c2
tayzar-v98 <tayzar@6ck.jp>
2021-03-19 22:48:38 +0900

    Copied from backup files

---
+++ src/views/Tabs.vue
@@ -0,0 +1,36 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <ion-tabs>
+        <ion-tab-bar slot="bottom">
+          <ion-tab-button tab="tab1" href="/tabs/tab1">
+            <ion-icon :icon="mail"/>
+            <ion-label>入力</ion-label>
+          </ion-tab-button>
+
+          <ion-tab-button tab="tab2" href="/tabs/tab2">
+            <ion-icon :icon="settings"/>
+            <ion-label>設定</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      </ion-tabs>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {IonContent, IonIcon, IonLabel, IonPage, IonTabBar, IonTabButton, IonTabs} from '@ionic/vue';
+import {calendarOutline, checkmarkCircle, images, mail, settings, square} from 'ionicons/icons';
+
+export default {
+  name: 'Tabs',
+  components: {IonContent, IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage},
+  setup() {
+    return {
+      images,
+      square,
+      calendarOutline, mail, checkmarkCircle, settings
+    }
+  }
+}
+</script>
