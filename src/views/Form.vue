534c1873235a7c3cbb8e1d3fbc5c54d942d01088
tayzar-v98 <tayzar@6ck.jp>
2021-03-20 22:04:29 +0900

    ガチャの情報入力画面を追加

---
+++ src/views/Form.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>宝箱の情報</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <div>{{ id }}</div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonItemDivider,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import {defineComponent, SetupContext} from "vue";
+import Modal from '@/components/Modal.vue';
+
+type Props = {
+  id: number;
+}
+export default defineComponent({
+  name: 'Form',
+  props: {
+    id: {
+      type: Number,
+      default: 0,
+    }
+  },
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage
+  },
+  setup(props: Props, context: SetupContext) {
+
+    return {}
+  }
+});
+</script>
