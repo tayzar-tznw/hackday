c47ef9bda7f9340be4a2b27cadc3883956073d8f
tayzar-v98 <tayzar@6ck.jp>
2021-03-20 22:02:30 +0900

    Fixed router path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,24 @@
 import {createRouter, createWebHistory} from '@ionic/vue-router';
 import {RouteRecordRaw} from 'vue-router';
-import Tabs from '../views/Tabs.vue'
+import GameSelection from '@/views/GameSelection.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
-        redirect: '/tabs/tab1'
+        component: GameSelection,
     },
     {
-        path: '/tabs/',
-        component: Tabs,
-        children: [
-            {
-                path: '',
-                redirect: '/tabs/tab1'
-            },
-            {
-                path: 'tab1',
-                component: () => import('@/views/Tab1.vue'),
-            },
-            {
-                path: 'tab2',
-                component: () => import('@/views/Tab2.vue'),
-            },
-        ]
-    }
+        path: '/mode',
+        component: () => import('@/views/ChooseMode.vue'),
+    },
+    {
+        path: '/form/:id',
+        component: () => import('@/views/Form.vue'),
+    },
+    {
+        path: '/result',
+        component: () => import('@/views/Result.vue'),
+    },
 ]
 
 const router = createRouter({
